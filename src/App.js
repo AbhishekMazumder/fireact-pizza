01e55dc40600a613c5217ab453ecf81d8c29838f
abhishek <abhishekmazumder69@gmail.com>
2021-06-20 10:47:10 +0530

    Feat: install styled-components, addglobal styles, add google fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: yellow;
+    font-family: 'Open Sans', sans-serif;
+  }
+  
+  h1,h2, h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello App</h1>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<div className="App">
+				<h1>Hello App</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque aut nihil in, sed nemo ipsa quis, eius ad rerum quod voluptatibus quasi hic deleniti illo, maiores natus esse tempore culpa?
+			</div>
+		</>
+	);
 }
 
 export default App;
