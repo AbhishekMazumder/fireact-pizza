aa3d28d6f94e431fd106148e54462c06337cd819
abhishek <abhishekmazumder69@gmail.com>
2021-06-20 15:20:52 +0530

    Add section to foodData and sorting the Menu and FoodItem accordingly

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
 			<Navbar />
       <Banner />
       <Menu />
-      <div>Content</div>
 		</>
 	);
 }
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-import {FoodGrid, FoodItem, FoodLabel} from "./FoodGrid"
-import { foodItems } from '../Data/foodData';
+import { FoodGrid, FoodItem, FoodLabel } from './FoodGrid';
+import { foods } from '../Data/foodData';
 
 const MenuStyled = styled.div`
-	/* border: 3px solid black; */
-	height: 550px;
+	border: 3px solid black;
+	height: auto;
 	margin: 0px 400px 50px 20px;
 `;
 
 function Menu() {
 	return (
 		<MenuStyled>
-			<h1>Our Menu</h1>
-			<FoodGrid>
-				{foodItems.map(food => (
-					<FoodItem img={food.img}>
-						<FoodLabel>{food.name}</FoodLabel>
-					</FoodItem>
-				))}
-			</FoodGrid>
+			{Object.entries(foods).map(([sectionName, foods]) => (
+				<>
+					<h1>{sectionName}</h1>
+					<FoodGrid>
+						{foods.map(food => (
+							<FoodItem img={food.img}>
+								<FoodLabel>{food.name}</FoodLabel>
+							</FoodItem>
+						))}
+					</FoodGrid>
+				</>
+			))}
 		</MenuStyled>
 	);
 }
